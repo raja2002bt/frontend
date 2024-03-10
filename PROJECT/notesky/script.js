954a74e792f06ab34882337c3bacc391f85a9114
Raja <144160739+raja2002bt@users.noreply.github.com>
2024-03-10 19:37:32 +0530

    Add files via upload

---
+++ PROJECT/notesky/script.js
@@ -0,0 +1,45 @@
+//selecting popupbox popupoverlay button
+var popupoverlay= document.querySelector(".popup-overlay")
+var popupbox= document.querySelector(".popup-box")
+var addpopupbutton=document.getElementById("add-popup-button")
+
+addpopupbutton.addEventListener("click",function(){
+    popupoverlay.style.display="block"
+    popupbox.style.display="block"
+})
+
+//select cancel button
+var cancelpopup=document.getElementById("cancel-popup")
+
+cancelpopup.addEventListener("click",function(event){
+    event.preventDefault()
+    popupoverlay.style.display="none"
+    popupbox.style.display="none"
+}
+)
+
+//select container addbook book-title-input book-author-input book-dscription-input
+
+var container=document.querySelector(".container")
+var addbook=document.getElementById("add-book")
+var booktitleinput=document.getElementById("book-title-input")
+var bookauthorinput=document.getElementById("book-author-input")
+var bookdscriptioninput=document.getElementById("book-dscription-input")
+
+addbook.addEventListener("click",function(event){
+    event.preventDefault()
+    var div=document.createElement("div")
+    div.setAttribute("class","book-container")
+    div.innerHTML=`<h2>${booktitleinput.value}</h2>
+    <h5>${bookauthorinput.value}</h5>
+    <p>${bookdscriptioninput.value}</p>
+    <button onclick="deletebook(event)">Delete</button>`
+    container.append(div)
+    popupoverlay.style.display="none"
+    popupbox.style.display="none"
+})
+
+function deletebook(event)
+{
+    event.target.parentElement.remove()
+}
