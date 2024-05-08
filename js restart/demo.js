3492daf6dad5b9969bea7771cee8e0358efeddea
Raja <144160739+raja2002bt@users.noreply.github.com>
2024-05-08 20:41:47 +0530

    Add files via upload

---
+++ js restart/demo.js
@@ -0,0 +1,207 @@
+// var a= 10
+// var b=30
+// var c=a+b
+// console.log(c)
+
+// var a = 10
+// var b = 30
+// function c(){
+//     console.log(a+b)
+// }
+// c()
+
+// var fdirector="Lokesh kanagaraj"
+// var fplayer="M S Dhoni"
+// var fmovie="12thFail"
+// function favourite()
+// {
+//     console.log("Favourite director :"+fdirector)
+//     console.log("Favourite player :"+fplayer)
+//     console.log("Favourite movie :"+fmovie)
+// }
+// favourite()
+
+// function area(length,breadth)
+// {
+//     var area=length*breadth
+//     console.log("Area is "+area+" sq ft")
+// }
+// area(10,40)
+
+// function sub(a,b)
+// {
+//     return (a-b)
+// }
+// console.log(sub(40,20))
+
+
+// function add(a,b)
+// {
+//     return(a+b) 
+// }
+// var total=add(10,50)
+// console.log(total)
+
+
+//  conditional statement
+
+// var homework=false
+// if(homework)
+// {
+//     console.log("Great job!")
+// }
+// else{
+//     console.log("Finesh the homework before playing")
+// }
+
+// var cookie=true
+// if(cookie)
+// {
+//     console.log("Would you like a cookie")
+// }
+// else
+// {
+//     console.log("Time to bake cookies")
+// }
+
+// var brush=false
+// if(brush)
+// {
+//     console.log('Your teeth are clean')
+// }
+// else
+// {
+//     console.log('Go brush your teeth now')
+// }
+
+// var birthday=true
+// if(birthday)
+// {
+//     console.log('Happy Birthday! Enjoy your special day!')
+// }
+// else
+// {
+//     console.log('Have a greate day!')
+// }
+
+// var rain=false
+// if (rain)
+// {
+//     console.log("Take an Umberla")
+// }
+// else
+// {
+//     console.log("Enjoy the sun shine")
+// }
+
+// var season="winter"
+// if (season =="spring")
+// {
+//     console.log("Enjoy the blooming flower.")
+// }
+// if (season == "summer")
+// {
+//     console.log("Have fun in sun.")
+// }
+// if (season == "autumn")
+// {
+//     console.log("Admire the colourful leaves.")
+// }
+// if ( season == "winter")
+// {
+//     console.log("Bundle up and stay warm")
+// }
+
+// var color="Red"
+// if (color =="green")
+// {
+//     console.log("Go..")
+// }
+// if (color == "Yellowr")
+// {
+//     console.log("Get ready.!")
+// }
+// if (color == "Red")
+// {
+//     console.log("Stop-")
+// }
+
+// var score= 65
+// if (score <= 50)
+// {
+//     console.log("You need to improve")
+// }
+// else if (score > 50 && score <= 70)
+// {
+//     console.log("Good job")
+// }
+// else if (score > 70)
+// {
+//     console.log("Excelent performance")
+// }
+
+// var number=7
+// if (number%2==0)
+// {
+//     console.log('Its an Even number')
+// }
+// else
+// (
+//     console.log("Its an odd number")
+// )
+
+// var Alphabet='a'
+// if (Alphabet== 'a'||Alphabet=='e'||Alphabet=='i'||Alphabet=='o'||Alphabet=='u'||Alphabet== 'A'||Alphabet=='E'||Alphabet=='I'||Alphabet=='O'||Alphabet=='U')
+// {
+//     console.log('Its a vowels')
+// }
+// else if ((Alphabet >='a' && Alphabet <='z'||Alphabet >='A' && Alphabet <='Z' )&&!(Alphabet== 'a'||Alphabet=='e'||Alphabet=='i'||Alphabet=='o'||Alphabet=='u'||Alphabet== 'A'||Alphabet=='E'||Alphabet=='I'||Alphabet=='O'||Alphabet=='U'))
+// {
+//     console.log("Its a consonants")
+// }
+// else
+// {
+//     console.log("Invalid Entery")
+// }
+
+// for(i=1;i<=5;i++)
+// {
+//     console.log("Raja")
+// }
+
+// for(i=1;i<=10;i++)
+// {
+//     console.log(i)
+// }
+
+
+// for(i=10;i>=1;i--)
+// {
+//     console.log(i)
+// }
+
+// for(i=1;i<=10;i=i+2)
+// {
+//     console.log(i)
+// }
+
+
+// for(i=2;i<=10;i=i+2)
+// {
+//     console.log(i)
+// }
+
+
+// for(i=1;i<=10;i++)
+// {
+//     n=i*2
+//     console.log(i+" x 2 = "+n)
+// }
+
+// DOM Manipulation
+
+//  var a= Math.random()
+//  console.log(Math.floor(a*10)+1)
+
+//  var b=Math.floor(6.75)
+//  console.log(b)
